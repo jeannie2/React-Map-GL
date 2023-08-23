37d43739c6c48e0ffd1a33efc9a1d28f4e3aca11
jeannie <jnam820@gmail.com>
2023-08-23 15:46:55 +0800

    control panel styles

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,8 +1,8 @@
 const Controls = () => {
   return (
     <div className="control-panel">
-      <h2>A map showing graffiti in NYC </h2>
-      <h3>(Source: <a href="https://data.cityofnewyork.us/Social-Services/graffiti/a26u-usa7?defaultRender=page">NYC Open Data API</a> + user-submitted data)</h3>
+      <h2>A map showing latest records of graffiti in New York City</h2>
+      <h3>(Data source: <a href="https://data.cityofnewyork.us/Social-Services/graffiti/a26u-usa7?defaultRender=page">NYC Open Data API</a> and user-submitted data)</h3>
     </div>
   );
 }
